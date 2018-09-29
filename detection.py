e034a8f64dbadb6d804082c190d7f01a3e623879
ibrahim <kekoreko3@gmail.com>
2018-09-29 22:49:43 +0200

    add closedhand.xml

--- detection.py
+++ detection.py
@@ -1,42 +1,35 @@
-import cv2
 import numpy as np
-
+import cv2
 
 cap = cv2.VideoCapture(0)
-#use trained xml
-face_cascade=cv2.CascadeClassifier('face.xml')
-eye_cascade=cv2.CascadeClassifier('eyes.xml')
-mouth_cascade=cv2.CascadeClassifier("mouth.xml")
 
-while 1:
+while (cap.isOpened()):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 
-    #capture frame by frame
-    ret, img = cap.read()
-    #convert the video into gray video without color
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #detect face,eyes and moouth in the video
-    faces = face_cascade.detectMultiScale(gray, 50, 50)
-    #Draw a rectangle boxes around face ,eye and mouth
-    for(x,y,w,h)in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
+    # Face and eye cascade
+    face_cascade = cv2.CascadeClassifier('eyes.xml')
+    eye_cascade = cv2.CascadeClassifier('eye.xml')
 
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        mouth = mouth_cascade.detectMultiScale(roi_color)
-        for(mx,my,mw,mh) in mouth:
-            cv2.rectangle(roi_color, (mx,my), (mx+mw,my+mh),(0,0,255), 5)
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-            for(ex,ey,ew,eh) in eyes:
-                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
+    # Show detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 1)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+        
+    # Display the resulting frame
+    cv2.imshow('OpenCV Basic Project', frame)
 
-                #Display the resuling frame
-                cv2.imshow('Frame',img)
-                k = cv2.waitKey(0)
-if k == 27:         # wait for ESC key to exit
-    cv2.destroyAllWindows()
-elif k == ord('s'): # wait for 's' key to save and exit
-    cv2.imwrite('messigray.png',img)
-    cv2.destroyAllWindows()    
+    # Close program by using key
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
 
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
